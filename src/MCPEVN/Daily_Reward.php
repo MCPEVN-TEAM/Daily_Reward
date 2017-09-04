b83a9dfb954f00a3fa88bb4d3f0036b51b1177cc
MCPEVN-TEAM <thai1134@outlook.com.vn>
2017-09-04 20:02:12 +0700

    Update Daily_Reward.php

--- src/MCPEVN/Daily_Reward.php
+++ src/MCPEVN/Daily_Reward.php
@@ -10,39 +10,51 @@ use pocketmine\utils\TextFormat;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
 
-class Daily_Reward extends PluginBase implements Listener{
+class Daily_Reward extends PluginBase implements Listener
+{
 
 
-   public function onEnable(){
-        if(!is_dir($this->getDataFolder()))	{
+   public function onEnable()
+   {
+        if(!is_dir($this->getDataFolder()))	
+	{
         mkdir($this->getDataFolder());
         }
-        $this->user = new Config($this->getDataFolder() ."user.yml", Config::YAML, []);//tạo config.yml
+        $this->user = new Config($this->getDataFolder() ."user.yml", Config::YAML, []);//t?o config.yml
     }
 
 
-    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-        if($cmd->getName()=="reward" or "rw"){
-       	 $ten = $sender->getName();
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args)
+    {
+        if($cmd->getName()=="reward" or "rw")
+		{
+       		$ten = $sender->getName();
         	$player = $sender->getServer()->getPlayer($ten);	
         	$date = date('Y-m-d');
         	if($this->user->exists($ten)){
-        		if($this->user->get($ten) !== $date){
+        		if($this->user->get($ten) !== $date)
+			{
         			$sender->getInventory()->addItem(Item::get(378, 0, 3));
-        			$sender->sendMessage('§b§o Chúc mừng bạn đã nhận quà hôm nay');
+        			$sender->sendMessage('§b§o Chúc m?ng b?n dã nh?n quà hôm nay');
         			$this->user->set($ten,$date);
         			$this->user->save();
+					return;
         		}
-        		else{
-        			$sender->sendMessage('§c§oHôm nay bạn đã nhận quà rồi');
-        	   }
-        	}else{
-       	 	$sender->getInventory()->addItem(Item::get(378, 0, 3));
-       	 	$sender->sendMessage('§b§o Chúc mừng bạn đã nhận quà hôm nay');
-       	 	$this->user->set($ten,$date);
-       	 	$this->user->save();
+        		else
+				{
+        			$sender->sendMessage('§c§oHôm nay b?n dã nh?n quà r?i');
+					return;
+        	   	}
+        	}
+			else
+			{
+				$sender->getInventory()->addItem(Item::get(378, 0, 3));
+				$sender->sendMessage('§b§o Chúc m?ng b?n dã nh?n quà hôm nay');
+				$this->user->set($ten,$date);
+				$this->user->save();
+				return;
      		}
 		}
-		return 'true';
+		return true;
     }
 }
